9f56a9a7551a13ef4c2f094f0ab39260c36cc680
Georgi-Tomov <95769826+Georgi-Tomov@users.noreply.github.com>
2021-12-12 17:07:07 +0200

    Add files via upload

---
+++ if.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+    int age;
+
+    printf("\nEnter your age: ");
+    scanf("%d",&age);
+
+    if(age >= 18){
+        printf("\nYou are signed up!\n");
+    }else if(age == 0){
+        printf("\nYou are just born.!\n");
+    }else if(age < 0){
+        printf("\nEnter valid age next time\n");
+    }
+    else{
+        printf("\nYou are not signed up!\n");
+    }
+
+    return 0;
+}
