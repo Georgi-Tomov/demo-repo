b9dd14f2b1ea4ed5647e02f9ea688323abdadd7c
Georgi-Tomov <95769826+Georgi-Tomov@users.noreply.github.com>
2021-12-12 17:07:32 +0200

    Add files via upload

---
+++ switch.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(){
+
+    char grade;
+
+    printf("Enter grade: \n");
+    scanf("%c",&grade);
+
+    switch(grade){
+        case 'A':
+            printf("Perfect!\n");
+            break;
+        case 'B':
+            printf("You did good!\n");
+            break;
+        case 'C':
+            printf("You did OK!\n");
+            break;
+        case 'D':
+            printf("At least it's not an F!\n");
+            break;
+        case 'F':
+            printf("You Failed!\n");
+            break;
+        default:
+            printf("Enter valid grade: \n");
+            break;
+        
+
+    }
+    return 0;
+}
