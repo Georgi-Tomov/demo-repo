90abc1bee4f5f6cc7abd09f33e4ec9840a8d791a
Georgi-Tomov <95769826+Georgi-Tomov@users.noreply.github.com>
2021-12-13 23:33:21 +0200

    Add files via upload

---
+++ stringFunc.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    char string1[] = "FName";
+    char string2[] = "LName";
+
+    strlwr(string1);
+    strupr(string1);
+    strcat(string1, string2);
+    strncat(string1, string2, 1);
+    strcpy(string1, string2);
+    strncpy(string1, string2, 1);
+
+    strset(string1, '?');
+    strnset(string1, 'x', 1);
+    strrev(string1);
+
+    printf("%s\n", string1);
+
+
+    return 0;
+}
