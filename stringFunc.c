8e5336b5b79637666edf0bd1ccab3ef8434adc76
Georgi-Tomov <mr.georgi.tomov@abv.bg>
2022-02-18 13:43:27 +0200

    Update stringFunc.c

--- stringFunc.c
+++ stringFunc.c
@@ -11,8 +11,11 @@ int main(){
     strupr(string1);
     
     strcat(string1, string2);
+    
     strncat(string1, string2, 1);
+    
     strcpy(string1, string2);
+    
     strncpy(string1, string2, 1);
 
     strset(string1, '?');
