582cc37581400a30d3ca4a21756f406340b55e9b
Georgi-Tomov <mr.georgi.tomov@abv.bg>
2022-04-02 00:55:33 +0300

    Update Task10.c

--- acexer/Task10.c
+++ acexer/Task10.c
@@ -13,7 +13,8 @@ void exit(int code);
 double caravanDue(double dDailyPriceCaravan, int iRentDays, int iReserved);
 double canperDue(double dDailyPriceCampers, int iRentDays, int iReserved);
 
-int main(){
+int main()
+{
     int iTotalCaravans = 3;
     int iTotalCampers = 3;
     int iReserved = 0;
