6cafa094c8e1ccad9d206021e520b9d7f6eed0e9
Georgi-Tomov <mr.georgi.tomov@abv.bg>
2022-05-06 20:24:21 +0300

    Update Task7.c

--- Task7.c
+++ Task7.c
@@ -9,17 +9,12 @@ int main()
 {
     unsigned short usValue = 24212;
     printf("usValue = %d\n", usValue);
-
-    
     int iValue = -1357674;
     printf("iValue = %d\n", iValue);
-
     int iValueOne = 1357674;
     printf("iValueOne = %d\n", iValueOne);
-    
     int iValueTwo = -1357674000;
     printf("iValueTwo = %d\n", iValueTwo);
-    
     unsigned int uiValue = 3657895000;
     printf("uiValue = %u\n", uiValue);
 
