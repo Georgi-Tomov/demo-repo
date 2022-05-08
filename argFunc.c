02472aecf6365e6537ae13618090e25539f74c5d
Georgi-Tomov <mr.georgi.tomov@abv.bg>
2022-05-08 19:23:31 +0300

    Update argFunc.c

--- argFunc.c
+++ argFunc.c
@@ -6,8 +6,11 @@ void birthday(char name[], int age)
     printf("%d is your age \n", age);
 }
 
-int main()
-{
+int main(){
+    
+    
+    
+    
     char name[] = "test";
     
     int age = 21;
