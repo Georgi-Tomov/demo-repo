776ff38f5303e60897d2b9c9f7cc0d5bb9ed40c5
Georgi-Tomov <mr.georgi.tomov@abv.bg>
2022-05-17 22:42:40 +0300

    Update Task6.c

--- Task6.c
+++ Task6.c
@@ -15,6 +15,13 @@ int main(){
     unsigned char ucValue = 255;
     printf("ucValue = %d\n",ucValue);
 
+    
+    
+    
+    
+    
+    
+    
     unsigned short usValue = 63498;
     printf("usValue = %d\n",usValue);
 
