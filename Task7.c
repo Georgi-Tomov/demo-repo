d1cd8350c1080e4cef09b81df4a67583e513176e
Georgi-Tomov <mr.georgi.tomov@abv.bg>
2023-09-08 00:17:56 +0300

    Update Task7.c

--- Task7.c
+++ Task7.c
@@ -11,7 +11,6 @@ int main(){
     
     int iValue = -1357674;
     printf("iValue = %d\n", iValue);
-    
     int iValueOne = 1357674;
     printf("iValueOne = %d\n", iValueOne);
     int iValueTwo = -1357674000;
