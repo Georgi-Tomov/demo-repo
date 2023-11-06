0c2d42e19cd568171b348c2f83e9f211fb6d6552
Georgi-Tomov <mr.georgi.tomov@abv.bg>
2023-11-07 00:34:43 +0200

    Update Task6.c

--- Task6.c
+++ Task6.c
@@ -14,7 +14,6 @@ int main()
     printf("ucValue = %d\n",ucValue);
     unsigned short usValue = 63498;
     printf("usValue = %d\n",usValue);
-    
     unsigned int uiValue  = 4294967292;
     printf("uiValue = %u\n", uiValue);
     signed long long sllValue = -9000000000000775845;
