3632905f3c72962a8c4d1300d571be17f7623ea2
Georgi-Tomov <mr.georgi.tomov@abv.bg>
2023-12-06 16:44:25 -0500

    Update switch.c

--- switch.c
+++ switch.c
@@ -28,6 +28,7 @@ int main()
         case 'F':
             printf("You Failed!\n");
             break;
+        
         default:
             printf("Enter valid grade: \n");
             break;
